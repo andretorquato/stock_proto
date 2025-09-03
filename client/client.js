8d86fa4535ffa3783962576f6f339bf7a0b23b7f
Andre Torquato <ats.torquato1@gmail.com>
2025-09-03 15:36:16 -0300

    call client streaming

--- client/client.js
+++ client/client.js
@@ -17,8 +17,30 @@ function main() {
     target,
     grpc.credentials.createInsecure()
   );
-  client.GetStockPrice({ symbol: "ABC" }, function (err, response) {
+  
+  var request = { symbol: "ABC" };
+  var deadline = new Date();
+  deadline.setSeconds(deadline.getSeconds() + 40);
+  client.GetStockPrice(request, { deadline }, function (err, response) {
     console.log(`Stock>`, response);
+  });
+
+  var call = client.GetStockPriceServerStreaming(request, { deadline });
+
+  call.on("data", function (response) {
+    console.log(`Stock Stream>`, response);
+  });
+
+  call.on("error", function (err) {
+    console.error(`Stock Stream> Error:`, err);
+  });
+
+  call.on("status", function (status) {
+    console.log(`Stock Stream> Status:`, status);
+  });
+
+  call.on("end", function () {
+    console.log(`Stock Stream> End`);
   });
 }
 
