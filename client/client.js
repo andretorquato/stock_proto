4e7316024a0653f93de1684a87738a900b3364c2
Andre Torquato <ats.torquato1@gmail.com>
2025-09-14 13:03:34 -0300

    client call update stock price streaming

--- client/client.js
+++ client/client.js
@@ -2,6 +2,9 @@ var PROTO_PATH = __dirname + "/protos/stock.proto";
 
 var grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
+var readline = require("readline");
+var fs = require("fs");
+
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
   longs: String,
@@ -17,30 +20,51 @@ function main() {
     target,
     grpc.credentials.createInsecure()
   );
-  
-  var request = { symbol: "ABC" };
-  var deadline = new Date();
-  deadline.setSeconds(deadline.getSeconds() + 40);
-  client.GetStockPrice(request, { deadline }, function (err, response) {
-    console.log(`Stock>`, response);
+
+  // var request = { symbol: "ABC" };
+  // var deadline = new Date();
+  // deadline.setSeconds(deadline.getSeconds() + 40);
+  // client.GetStockPrice(request, { deadline }, function (err, response) {
+  //   console.log(`Stock>`, response);
+  // });
+
+  // var call = client.GetStockPriceServerStreaming(request, { deadline });
+
+  // call.on("data", function (response) {
+  //   console.log(`Stock Stream>`, response);
+  // });
+
+  // call.on("error", function (err) {
+  //   console.error(`Stock Stream> Error:`, err);
+  // });
+
+  // call.on("status", function (status) {
+  //   console.log(`Stock Stream> Status:`, status);
+  // });
+
+  // call.on("end", function () {
+  //   console.log(`Stock Stream> End`);
+  // });
+
+  var call = client.updateStockPriceClientStreaming(function (err, response) {
+    if (err) {
+      console.error(`Update Stock> Error:`, err);
+    } else {
+      console.log(`Update Stock>`, response);
+    }
   });
 
-  var call = client.GetStockPriceServerStreaming(request, { deadline });
-
-  call.on("data", function (response) {
-    console.log(`Stock Stream>`, response);
+  var fileStream = fs.createReadStream(__dirname + "/data/stock.txt");
+  var rl = readline.createInterface({ input: fileStream });
+  rl.on("line", function (line) {
+    console.log(`Update Stock>`, line);
+    var stock = JSON.parse(line);
+    call.write(stock);
   });
 
-  call.on("error", function (err) {
-    console.error(`Stock Stream> Error:`, err);
-  });
-
-  call.on("status", function (status) {
-    console.log(`Stock Stream> Status:`, status);
-  });
-
-  call.on("end", function () {
-    console.log(`Stock Stream> End`);
+  rl.on("close", function () {
+    console.log("Update Stock> All data sent.");
+    call.end();
   });
 }
 
